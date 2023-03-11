5e365d867ba321b19996a0b832f466fb911f8755
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-03-11 15:11:23 -0500

    update project on file change

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { workspace } from 'vscode';
 import { loadBase, getInstance } from './ibmi';
-import ProjectExplorer from './views/projectExplorer';
+import ProjectExplorer, { ProjectManager } from './views/projectExplorer';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-ibm-projectmode" is now active!');
@@ -27,9 +28,21 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	const projectWatcher = vscode.workspace.createFileSystemWatcher(`**/*.{env,json}`);
-	projectWatcher.onDidChange(() => {projectExplorer.refresh();});
-	projectWatcher.onDidCreate(() => {projectExplorer.refresh();});
-	projectWatcher.onDidDelete(() => {projectExplorer.refresh();});
+	projectWatcher.onDidChange(async (uri) => {
+		const workspaceFolders = workspace.workspaceFolders;
+		if (workspaceFolders && workspaceFolders.length > 0) {
+			const worksapceFolder = workspaceFolders.filter(workspaceFolder =>
+				uri.fsPath.startsWith(workspaceFolder.uri.fsPath)
+			)[0];
+			const iProject = ProjectManager.get(worksapceFolder);
+			if (iProject) {
+				await iProject.read();
+			}
+		}
+		projectExplorer.refresh();
+	});
+	projectWatcher.onDidCreate(() => { projectExplorer.refresh(); });
+	projectWatcher.onDidDelete(() => { projectExplorer.refresh(); });
 
 	context.subscriptions.push(
 		vscode.window.registerTreeDataProvider(
@@ -40,4 +53,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
