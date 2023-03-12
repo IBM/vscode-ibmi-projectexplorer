4db3ed4eb1ee7bddb7008c54fb179bc412cfefde
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-03-11 19:55:38 -0500

    add job log view

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { loadBase, getInstance } from './ibmi';
+import JobLog from './views/jobLog';
 import ProjectExplorer from './views/projectExplorer';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-ibm-projectmode" is now active!');
@@ -27,17 +28,28 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	const projectWatcher = vscode.workspace.createFileSystemWatcher(`**/*.{env,json}`);
-	projectWatcher.onDidChange(() => {projectExplorer.refresh();});
-	projectWatcher.onDidCreate(() => {projectExplorer.refresh();});
-	projectWatcher.onDidDelete(() => {projectExplorer.refresh();});
+	projectWatcher.onDidChange(() => { projectExplorer.refresh(); });
+	projectWatcher.onDidCreate(() => { projectExplorer.refresh(); });
+	projectWatcher.onDidDelete(() => { projectExplorer.refresh(); });
+
+	const jobLog = new JobLog(context);
+
+	const jobLogWatcher = vscode.workspace.createFileSystemWatcher(`**/*.logs/{joblog.json,output.log}`);
+	jobLogWatcher.onDidChange(() => { jobLog.refresh(); });
+	jobLogWatcher.onDidCreate(() => { jobLog.refresh(); });
+	jobLogWatcher.onDidDelete(() => { jobLog.refresh(); });
 
 	context.subscriptions.push(
 		vscode.window.registerTreeDataProvider(
 			`projectExplorer`,
 			projectExplorer
+		),
+		vscode.window.registerTreeDataProvider(
+			`jobLog`,
+			jobLog
 		)
 	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
