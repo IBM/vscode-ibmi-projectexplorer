b7b3d973df76acde6208c8b964d1e51d0460888f
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-03-14 22:06:20 -0400

    register command

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import ProjectExplorer from './views/projectExplorer';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-ibm-projectmode" is now active!');
@@ -27,17 +27,43 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	const projectWatcher = vscode.workspace.createFileSystemWatcher(`**/*.{env,json}`);
-	projectWatcher.onDidChange(() => {projectExplorer.refresh();});
-	projectWatcher.onDidCreate(() => {projectExplorer.refresh();});
-	projectWatcher.onDidDelete(() => {projectExplorer.refresh();});
+	projectWatcher.onDidChange(() => { projectExplorer.refresh(); });
+	projectWatcher.onDidCreate(() => { projectExplorer.refresh(); });
+	projectWatcher.onDidDelete(() => { projectExplorer.refresh(); });
 
+	// Tree views
 	context.subscriptions.push(
 		vscode.window.registerTreeDataProvider(
 			`projectExplorer`,
 			projectExplorer
 		)
 	);
+
+	// Commands
+	context.subscriptions.push(
+		vscode.commands.registerCommand(`vscode-ibmi-projectmode.addToIncludes`, (element: vscode.TreeItem) => {
+			const path = (element as any).path;
+			if (path) {
+				const ibmi = getInstance();
+				const deploymentDirs = ibmi?.getStorage().getDeployment()!;
+				const localDir = projectElement.resourceUri?.path!; //TODO: Fix
+				const remoteDir = deploymentDirs[localDir]; //TODO: Fix
+
+				const iProject = ProjectManager.get(projectElement.workspaceFolder); //TODO: Fix
+				if (remoteDir && remoteDir !== path && path.startsWith(remoteDir)) {
+					//Add relative path to remoteDir to include paths
+					const relativePath = path.relative(remoteDir, path);
+					iProject.addToIncludes(relativePath);
+				} else {
+					//Add absolute path to include paths
+					iProject.addToIncludes(path);
+				}
+			} else {
+				vscode.window.showErrorMessage('Failed to retrieve path to directory.');
+			}
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
