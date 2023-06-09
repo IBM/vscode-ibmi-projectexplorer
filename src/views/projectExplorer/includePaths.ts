bd0501e7961c529e5b154ea388b8d0656dfcadfc
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2023-06-09 15:54:06 -0400

    update to getDeployDir API
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/views/projectExplorer/includePaths.ts
+++ src/views/projectExplorer/includePaths.ts
@@ -59,13 +59,13 @@ export default class IncludePaths extends ProjectExplorerTreeItem {
             // Relative local include path
             items.push(new LocalIncludePath(this.workspaceFolder, includePath, includePathUri, position));
           } catch (e) {
-            if (includePath.startsWith('/')) {
+            const deployDir = iProject!.getDeployDir();
+            if (includePath.startsWith('/') || !deployDir) {
               // Absolute remote include path
               items.push(new RemoteIncludePath(this.workspaceFolder, includePath, position));
             } else {
               // Relative remote include path
-              const remoteDir = await iProject!.getRemoteDir();
-              const absoluteIncludePath = path.posix.join(remoteDir, includePath);
+              const absoluteIncludePath = path.posix.join(deployDir, includePath);
               items.push(new RemoteIncludePath(this.workspaceFolder, absoluteIncludePath, position, { label: includePath }));
             }
           }
