1ec31af7c0d17b91627398a3c0510f83416bc4fa
Irfan Sharif <IrfanSharif@ibm.com>
2023-09-26 13:20:23 -0400

    Update filter by severity to apply for all commands
    Signed-off-by: Irfan Sharif <IrfanSharif@ibm.com>

--- src/views/jobLog/log.ts
+++ src/views/jobLog/log.ts
@@ -15,6 +15,7 @@ export default class Log extends TreeItem implements ProjectExplorerTreeItem {
   static contextValue = ContextValue.log;
   jobLogInfo: JobLogInfo;
   onlyShowFailedCommands : boolean;
+  showSeverityLevels: number;
 
   constructor(public workspaceFolder: WorkspaceFolder, jobLogInfo: JobLogInfo, isLocal: boolean = false) {
     super(jobLogInfo.createdTime.toLocaleString(), TreeItemCollapsibleState.Collapsed);
@@ -23,10 +24,15 @@ export default class Log extends TreeItem implements ProjectExplorerTreeItem {
     this.iconPath = new ThemeIcon('archive', isLocal ? new ThemeColor('joblog.local') : undefined);
     this.contextValue = Log.contextValue;
     this.onlyShowFailedCommands = false;
+    this.showSeverityLevels = 0;
   }
 
   toggleShowFailed(): void {
     this.onlyShowFailedCommands = this.onlyShowFailedCommands === false ? true : false;
+  }
+
+  setSeverityLevel(severityLevel: number): void {
+    this.showSeverityLevels = severityLevel;
   }
 
   getChildren(): ProjectExplorerTreeItem[] {
@@ -38,11 +44,11 @@ export default class Log extends TreeItem implements ProjectExplorerTreeItem {
       items.push(...jobLogInfo.commands?.filter(
         command => command.failed
       ).map(
-        command => new Command(this.workspaceFolder, command)
+        command => new Command(this.workspaceFolder, command, this.showSeverityLevels)
       ));
     }else{
       items.push(...jobLogInfo.commands?.map(
-        command => new Command(this.workspaceFolder, command)
+        command => new Command(this.workspaceFolder, command, this.showSeverityLevels)
       ));
     }
     return items;
