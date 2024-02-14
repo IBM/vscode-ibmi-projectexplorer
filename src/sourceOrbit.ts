2e72c03e6ca73128924bc1a1a1404c49d9fbcd1b
worksofliam <mrliamallan@live.co.uk>
2024-02-14 11:16:19 -0500

    Hide SO migration options if not installed
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

---
+++ src/sourceOrbit.ts
@@ -0,0 +1,20 @@
+/*
+ * (c) Copyright IBM Corp. 2024
+ */
+
+import { Extension, extensions } from "vscode";
+
+let baseExtension: Extension<any> | undefined;
+
+export async function sourceOrbitEnabled(): Promise<boolean> {
+  if (!baseExtension) {
+    baseExtension = (extensions ? extensions.getExtension(`IBM.vscode-sourceorbit`) : undefined);
+  }
+
+  if (baseExtension && !baseExtension.isActive) {
+    await baseExtension.activate();
+  }
+
+  // Source Orbit extension does not have an accessible API. We just want to know if we have it installed or not.
+  return (baseExtension && baseExtension.isActive && baseExtension.exports ? true : false);
+}
